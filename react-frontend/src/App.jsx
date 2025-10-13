c33a017ec1dcce792b78fc4095589fd3008468b8
tranvietgiang <tranvietgiang25@gmail.com>
2025-10-13 23:47:31 +0700

    test-import list student

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import GetUser from "./Components/PageOther/GetUser";
+import ImportStudents from "./Components/Features/ImportListStudents/ImportStudents";
+import Login from "./Components/Auth/Login";
 
 function App() {
   return (
@@ -8,7 +10,11 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route path="/nckh_ng" element={<GetUser />} />
+            <Route path="/nckh-ng" element={<ImportStudents />} />
+          </Routes>
+
+          <Routes>
+            <Route path="/nckh-login" element={<Login />} />
           </Routes>
         </div>
       </Router>
